fa4c7e80c8d75abcad5e46c20ac674db9156f9a3
Tadas Petra <tadas@tadaspetra.com>
2024-05-04 09:46:16 -0500

    Base site with landing

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const SITE = {
+    title: "Tadas Petra",
+    description: "Tadas Petra personal blog",
+};
+
+export const SOCIALS = {
+    github: "https://github.com/tadaspetra",
+    x: "https://twitter.com/@tadaspetra",
+    youtube: "https://youtube.com/@tadaspetra",
+}
+
