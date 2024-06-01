95b7bd7ce27b2c5668a8b61c7fd40ce68342f89e
Tadas Petra <tadas@tadaspetra.com>
2024-06-01 12:06:00 -0500

    does it work on server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [tailwind(), react(), db()]
 });
+
